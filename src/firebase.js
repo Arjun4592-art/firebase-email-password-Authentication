ddb2ef70ba113f4b1303b6c49cf2b88820835b36
Arjun Prasad <arjunmishra769@gmail.com>
2026-01-02 19:35:57 +0530

    first Commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,20 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import process from 'process'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDqmwznb5FoG0CS07rjIILd-2kNkEZYNO0',
-  authDomain: 'fir-demo-a0250.firebaseapp.com',
-  projectId: 'fir-demo-a0250',
-  storageBucket: 'fir-demo-a0250.firebasestorage.app',
-  messagingSenderId: '377016076522',
-  appId: '1:377016076522:web:6d11491979a63a2dd92f93',
-  measurementId: 'G-RFJRFLCVGE',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
